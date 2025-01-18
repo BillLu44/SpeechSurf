1daacfc4a71c85756de495d5a5b2a15dc77fc698
Sean-Chen71 <138247543+SeanChen2@users.noreply.github.com>
2025-01-18 03:37:25 -0500

    Added logo

--- landing_page/Voice-App/src/Components/Navbar.jsx
+++ landing_page/Voice-App/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import "../App.css"
 export default function Navbar() {
     return (
         <div className="navbar">
-            <div className="nav-logo"><img src="/SpeechSurf.svg"/></div>
+            <div className="nav-logo"><img src="/SpeechSurf.png"/></div>
             <Link to="/" className="nav-title">SpeechSurf</Link>
             <ul className="nav-btn-container">
                 <li><Link to="/">HOME</Link></li>
