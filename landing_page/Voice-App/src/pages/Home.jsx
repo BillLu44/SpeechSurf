432b250efe378158c97ff9a148a5d9f7f4e2cf66
Sean Chen <seanchen.sc2@gmail.com>
2025-07-26 23:42:16 -0400

    Make new build. Fix releases link.

--- landing_page/Voice-App/src/pages/Home.jsx
+++ landing_page/Voice-App/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
             <div className="landing-desc">Download now for a hands-off browsing experience, powered by speech recognition and AI.</div>
 
-            <a href="https://github.com/BillLu44/SpeechSurf/releases/tag/v1.0.0" download="SpeechSurf.zip">
+            <a href="https://github.com/BillLu44/SpeechSurf/releases" download="SpeechSurf.zip">
                 <button className="download-btn">DOWNLOAD</button>
             </a>
 
